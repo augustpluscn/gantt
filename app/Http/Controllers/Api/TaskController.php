390112d5de81427a4616bce7b67129f0e574413c
august <1@augustor.com>
2022-04-09 14:38:03 +0800

    排序

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -8,7 +8,11 @@ class TaskController extends Controller
 {
     public function taskList()
     {
-        $task = DB::connection('cus')->table('V_task_detail')->get();
+        $task = DB::connection('cus')
+            ->table('V_task_detail')
+            ->orderBy("机台系统编号")
+            ->orderBy("排序")
+            ->get();
 
         return $this->success([
             'task' => $task,
